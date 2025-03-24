cfd2a624beebf4d252b6b7c98cb11fa6af207057
Beatriz <b.tmarioti3.0@gmail.com>
2025-03-24 12:19:56 -0300

    Trocando as imagens de local para o link

--- Página carrinho/script.js
+++ Página carrinho/script.js
@@ -1,3 +1,18 @@
+// side bar 
+document.addEventListener("DOMContentLoaded", function () { //As modificações no DOM podem ser feitas, como adicionar ou alterar elementos, adicionar eventos, etc
+    let menuIcon = document.getElementById("menuIcon");
+    let sidebar = document.getElementById("sidebar");
+    let closeBtn = document.getElementById("closeBtn");
+
+    menuIcon.addEventListener("click", function () {  // ele adiciona uma função quando o menuIcon eh clicado
+        sidebar.style.left = "0"; //abrindo a side - mudando a estilização do sidebar left para 0
+    });
+
+    closeBtn.addEventListener("click", function () {// ele adiciona uma função quando o closeBtn eh clicado
+        sidebar.style.left = "-300px";//fechando a side -  mudando a estilização do sidebar left para -300 - escondendo como no inicio
+    });
+});
+
 const buyAllButton = document.querySelector('.btn');
         const popup = document.getElementById('popup');
         const caixa = document.getElementById('caixa');
