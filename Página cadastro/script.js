76318e293c11d58f4dba5cd1a93c4f5d76cb2491
Beatriz <b.tmarioti3.0@gmail.com>
2025-03-28 07:40:42 -0300

    Adicionando js

--- Página cadastro/script.js
+++ Página cadastro/script.js
@@ -2,13 +2,52 @@ const form = document.getElementById('formulario-container');
         const popup = document.getElementById('popup');
         const caixa = document.getElementById('caixa');
         const fecharPopup = document.getElementById('fecha-popup');
+        function doPost(url, body){
+            console.log(body)
+            let request = new XMLHttpRequest()
+            request.open("POST", url, true)
+            request.setRequestHeader("Content-type", "application/json")
+            request.send(JSON.stringify(body))
+        
+            request.onload = function(){
+                console.log(this.responseText)
+            }
+        
+            return request.responseText
+        }
+        
+        function cadastrarUsuario(){
+            event.preventDefault()
+            let url = "https://bytestore-eddr.onrender.com/usuario/"
+        
+            let nome = document.getElementById('name').value
+            let email = document.getElementById('email').value
+            let password = document.getElementById('password').value
+        
+            localStorage.setItem("nome", nome)
+            localStorage.setItem("email", email)
+            localStorage.setItem("password", password)
+        
+            body = {
+                "name": localStorage.getItem("nome"),
+                "email": localStorage.getItem("email"),
+                "password": localStorage.getItem("password")
+            }
+            
+            console.log(body)
+            doPost(url, body)
+            console.log(localStorage.getItem("email"))
+        }
 
         form.addEventListener('submit', function(event) {
             event.preventDefault();
 
+            
+            if(cadastrarUsuario){
             // Exibir o popup
-            popup.style.display = 'block';
-            caixa.style.display = 'block';
+                popup.style.display = 'block';
+                caixa.style.display = 'block';
+            }
         });
 
         fecharPopup.addEventListener('click', function() {
