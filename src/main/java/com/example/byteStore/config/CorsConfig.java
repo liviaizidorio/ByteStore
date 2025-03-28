4a6ea80a27660c488f97386f16df6ccd6c7be6b8
Lívia Izidório <liviaizidorio.li@gmail.com>
2025-03-28 09:48:34 -0300

    Implementa nova classe de configuração.

---
+++ src/main/java/com/example/byteStore/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.example.byteStore.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/usuario/**")
+                        .allowedOrigins("https://bytestore-02o5.onrender.com")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
